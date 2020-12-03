d20e46efb19f80d42db937d647be12f25c611caa
Nitai Charan <nitaicharan@gmail.com>
2020-12-03 17:42:05 -0300

    196. Consulta simples com @Query

--- src/main/java/com/nitaicharan/udemy_sbhrestijwts3msqlmdb5/repository/PostRepository.java
+++ src/main/java/com/nitaicharan/udemy_sbhrestijwts3msqlmdb5/repository/PostRepository.java
@@ -5,9 +5,13 @@ import java.util.List;
 import com.nitaicharan.udemy_sbhrestijwts3msqlmdb5.domain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
